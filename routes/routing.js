7f3b7a77df3b5c3094b380f7b07c6e55cdcb7b4f
jakepwheeler <jakephilipwheeler@gmail.com>
2022-03-21 17:11:27 +0000

    routing update with .ejs extention

--- routes/routing.js
+++ routes/routing.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index.ejs');
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login.ejs');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register.ejs');
 });
 
 router.get('/doodlPage', function(req, res, next) {
-  res.render('doodlPage');
+  res.render('doodlPage.ejs');
 });
 
 router.get('/gallery', function(req, res, next) {
-  res.render('gallery');
+  res.render('gallery.ejs');
 });
 
 module.exports = router;
