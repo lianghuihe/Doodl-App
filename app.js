42046687fdc4ab0ec43019deffde8ebe1e4eef67
jakepwheeler <jakephilipwheeler@gmail.com>
2022-03-22 15:38:05 +0000

    update to passport functionality

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/routing');
 var app = express();
+const flash = require('express-flash')
+const session = require('express-session')
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://doadmin:58QvrM41C390iFz6@db-mongodb-lon1-64588-a6408448.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-lon1-64588&tls=true&tlsCAFile=" +  path.join(__dirname,'ca-certificate.crt');
 const bcrypt = require('bcrypt')
@@ -33,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash())
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
