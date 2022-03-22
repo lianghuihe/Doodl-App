1c7afb670102556a7d44438a8a420a3bb9cef3a3
jakepwheeler <jakephilipwheeler@gmail.com>
2022-03-22 23:09:13 +0000

    merge error correction

--- routes/routing.js
+++ routes/routing.js
@@ -37,13 +37,43 @@ router.get('/voting', function(req, res, next) {
   res.render('voting.ejs');
 });
 
-router.post('/register',
-  passport.authenticate('register', { session: false }),
-  async (req, res, next) => {
-    res.json({
-      message: 'Signup successful',
-      user: req.user
-    });
+router.post('/api/register', async (req, res, next) => {
+  const { username, email, password: plainTextPassword } = req.body
+
+  if (!username || typeof username !== 'string') {
+    return res.json({ status: 'error', error: 'Invalid username' })
+  }
+
+  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+    return res.json({ status: 'error', error: 'Invalid password' })
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: 'error',
+      error: 'Password too small. Should be atleast 6 characters'
+    })
+  }
+
+  const password = await bcrypt.hash(plainTextPassword, 10)
+
+  try {
+    const response = await User.create({
+      email,
+      username,
+      password
+    })
+    console.log('User created successfully: ', response)
+  } catch (error) {
+    if (error.code === 11000) {
+      // duplicate key
+      return res.json({ status: 'error', error: 'Username already in use' })
+    }
+    throw error
+  }
+
+  res.json({ status: 'ok' })
+
   }
 );
 
