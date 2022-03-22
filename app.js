d77d68243c29b723bf48a2f27c32dc9660536441
jakepwheeler <jakephilipwheeler@gmail.com>
2022-03-22 12:15:02 +0000

    Added database connection

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/routing');
 var app = express();
+const uri = "mongodb+srv://doadmin:58QvrM41C390iFz6@db-mongodb-lon1-64588-a6408448.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-lon1-64588&tls=true&tlsCAFile=" +  path.join(__dirname,'ca-certificate.crt');
+
+//Connect to database
+mongoose.connect(uri);
+const db = mongoose.connection;
+
 app.set('views', path.join(__dirname, 'views')); // view engine setup
 app.set('view engine', 'ejs');
 
@@ -14,6 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//test connection to mongoDB
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.use('/', indexRouter);
 
