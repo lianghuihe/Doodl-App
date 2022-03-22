d601986440e253a455f55bc8e8cb457c8e74e536
jakepwheeler <jakephilipwheeler@gmail.com>
2022-03-22 21:18:00 +0000

    correct call to password input box

--- public/js/register.js
+++ public/js/register.js
@@ -3,12 +3,12 @@ var user;
 
 userInput.addEventListener('click', e => {
     if (e.target.id === 'register') {
-        let passwd = (document.getElementsByName('Password')).value;
-        let confirmPasswd = (document.getElementsByName('ConfirmPassword')).value;
+        let passwd = (document.getElementById('Password')).value;
+        let confirmPasswd = (document.getElementById('ConfirmPassword')).value;
         if (passwd === confirmPasswd){
             user = {
-                userName: (document.getElementsByName('UserName')).value,
-                password: (document.getElementsByName('Password')).value,
+                userName: (document.getElementById('UserName')).value,
+                password: (document.getElementById('Password')).value,
             }
             document.location='doodlPage';
         }
