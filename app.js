2c70e190f67fe9336a93ab4645706a73146a3695
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-23 17:31:21 +0100

    i have no idea what the issue is here

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './env' })
+require('dotenv').config({ path: './public/env' })
 const csv = require('csv-parser')
 const fs = require('fs')
 var createError = require('http-errors');
