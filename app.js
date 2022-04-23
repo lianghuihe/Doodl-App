14e44a6a88bfc68cba0897f2924216b93d54cfa4
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-23 17:25:46 +0100

    .env path

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './env/.env' })
+require('dotenv').config({ path: './env' })
 const csv = require('csv-parser')
 const fs = require('fs')
 var createError = require('http-errors');
