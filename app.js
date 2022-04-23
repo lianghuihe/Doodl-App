b7c3c623077b78c03d8e8187cf7e75e63e7e7d28
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-23 17:48:21 +0100

    file paths

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ const mongoose = require('mongoose');
 const User = require("./model/User");
 const uri = "mongodb+srv://doadmin:58QvrM41C390iFz6@db-mongodb-lon1-64588-a6408448.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-lon1-64588&tls=true&tlsCAFile=" +  path.join(__dirname,'ca-certificate.crt');
 const passport = require('passport');
-const {
-  checkAuthenticated,
-  checkNotAuthenticated,
-} = require("./auth/auth");
 
 /*const initializePassport = require('./passport-config');
 initializePassport(
