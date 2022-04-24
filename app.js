cd6e9abafde4c470199e68ef4ae07931aab83e21
Rohan Sharma <rohansharma@unknown.home>
2022-04-24 20:03:40 +0100

    Locating error

--- app.js
+++ app.js
@@ -98,10 +98,11 @@ app.use(function(err, req, res, next) {
 });
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+*/
 app.get('/', checkAuthenticated, function(req, res, next) {
   res.render('index.ejs');
 });
