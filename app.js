a28bdc64bbefa182ec4499e03c50cf65371de592
Rohan Sharma <rohansharma@rohans-air.home>
2022-04-24 19:23:06 +0100

    looking for error

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ const mongoose = require('mongoose');
 const User = require("./model/User");
 const uri = "mongodb+srv://doadmin:58QvrM41C390iFz6@db-mongodb-lon1-64588-a6408448.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-lon1-64588&tls=true&tlsCAFile=" +  path.join(__dirname,'ca-certificate.crt');
 const passport = require('passport');
-
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("./auth/auth");
 
 const initializePassport = require("./passport-config");
 initializePassport(
@@ -99,4 +102,77 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('/', checkAuthenticated, function(req, res, next) {
+  res.render('index.ejs');
+});
+
+app.get('views/login.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('login.ejs');
+});
+
+app.get('views/register.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('register.ejs');
+});
+
+app.get('views/doodlPage.ejs', checkNotAuthenticated, function(req, res, next) {
+  var currentPrompt = req.app.locals.currentPrompt;
+  res.render('doodlPage.ejs', {currentPrompt : currentPrompt} );
+});
+
+app.get('views/gallery.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('gallery.ejs');
+});
+
+app.get('views/gdprPage.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('gdprPage.ejs');
+});
+
+app.get('views/report.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('report.ejs');
+});
+
+app.get('views/voting.ejs', checkNotAuthenticated, function(req, res, next) {
+  res.render('voting.ejs');
+});
+
+app.post("views/register.ejs", checkNotAuthenticated, async (req, res) => {
+  const userFound = await User.findOne({ email: req.body.email });
+
+  if (userFound) {
+    req.flash("error", "User with that email already exists");
+    res.redirect("views/register.ejs");
+  } else {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+      res.redirect("views/login.ejs");
+    } catch (error) {
+      console.log(error);
+      res.redirect("views/register.ejs");
+    }
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("views/login.ejs");
+});
+
+app.post(
+  "views/login.ejs",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "views/doodlPage.ejs",
+    failureRedirect: "views/login.ejs",
+    failureFlash: true,
+  })
+);
+
+//module.exports = router;
 module.exports = app;
