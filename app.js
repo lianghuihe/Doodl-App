4afaa7f2a0a1fa9fd4302cb5f4eb952b8c4a2987
Rohan Sharma <rohansharma@rohans-air.home>
2022-04-24 16:21:09 +0100

    Minor changes

--- app.js
+++ app.js
@@ -62,10 +62,9 @@ fs.createReadStream('./public/wordList.csv')
 app.set('views', path.join(__dirname, 'views')); // view engine setup
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
