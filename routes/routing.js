cfe1bde40ca60c18ea9ebd0fc8af6dfbe6185cbe
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-25 19:19:02 +0100

    schema paths

--- routes/routing.js
+++ routes/routing.js
@@ -2,8 +2,8 @@ require('dotenv').config({path: 'env/.env'});
 const express = require('express');
 var path = require('path');
 const bcrypt = require("bcryptjs");
-const User = require("../model/user.js");
-const Doodl = require("../model/doodl.js");
+const User = require("../model/User");
+const Doodl = require("../model/Doodl");
 const passport = require('passport');
 const router = express();
 const {
