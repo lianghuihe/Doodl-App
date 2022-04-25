227406a6c17eba5a87de2d96f678f28efa9cdfcc
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-25 16:24:15 +0100

    password config

--- passport-config.js
+++ passport-config.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail, getUserById){
     const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email)
+        const user = await getUserByEmail(email)
         if(user == null){
             return done(null, false, {message: 'No user with that email'})
         }
 
         try{
-            if(await bcrypt.compare(password, user.password)){
+            if(await bcrypt.compare(password, await user.password)){
                 return done(null, user)
             }else{
                 return done(null, false, {message: 'Password incorrect'})
