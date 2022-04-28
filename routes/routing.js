c034ce355bfeeed0e7cadcb41e64c99a8d339ebf
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-29 00:07:47 +0100

    new way to populate form

--- routes/routing.js
+++ routes/routing.js
@@ -133,7 +133,7 @@ router.post("/doodlPage", checkAuthenticated, async (req, res) => {
 });
 
 router.post("/like", checkAuthenticated, async (req, res) => {
-  console.log(req);
+  console.log(req.body);
   const likeFound = await User.findOne({ username: req.body.email, doodlID: req.body.likeDoodlID, type: req.body.likeType});
 
   if(likeFound){
