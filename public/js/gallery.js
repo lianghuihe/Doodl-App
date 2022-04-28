387017f72ec6a480b53928fb01355a1a8769bd87
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-28 18:44:28 +0100

    new methodology for image drawing

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,10 +1,19 @@
+var table = document.createElement('table');
 var doodlData;
+var canvases=[];
+var contexts=[];
 doodlData = document.getElementById('doodlData').value;
 
 var doodlArray = [];
 doodlArray = doodlData.split('||');
+for (var i = 1; i < (doodlArray.length / 2); i++){
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
 
-var table = document.createElement('table');
+    canvases[i] = canvas;
+    contexts[i] = context;
+}
+
 for (var i = 1; i < doodlArray.length; i++){
     var tr = document.createElement('tr');
     var tempArray = doodlArray[i].split('|', 2);
@@ -12,20 +21,13 @@ for (var i = 1; i < doodlArray.length; i++){
     var td1 = document.createElement('td');
     var td2 = document.createElement('td');
 
-    let url = tempArray[1];
-    let doodlCanvas = document.createElement('canvas');
-    let ctx = doodlCanvas.getContext('2d');
-    let img = new Image;
-    await new Promise(r => img.onload=r, img.src=url);
-    img.onload = function(){
-        ctx.drawImage(img,0,0); // Or at whatever offset you like
-    };
-
-    var text1 = document.createTextNode(tempArray[0]);
-    var text2 = doodlCanvas;
-
-    td1.appendChild(text1);
-    td2.appendChild(text2);
+    var image = new Image;
+    image.src = tempArray[1];
+    contexts[i].drawImage(img,0,0);
+  
+ 
+    td1.appendChild(document.createTextNode(tempArray[0]));
+    td2.appendChild(canvases[i]);
     tr.appendChild(td1);
     tr.appendChild(td2);
 
