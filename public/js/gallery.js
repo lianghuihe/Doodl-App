0172144b5eea785deed52ff9af669f54cfc229bd
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-28 17:42:44 +0100

    dynamically generate doodl table initial attempt

---
+++ public/js/gallery.js
@@ -0,0 +1,25 @@
+var doodlData;
+doodlData = document.getElementById('doodlData').value;
+
+var doodlArray = [];
+doodlArray = doodlData.split(',');
+
+var table = document.createElement('table');
+for (var i = 1; i < (doodlArray.length / 2); i++){
+    var tr = document.createElement('tr');   
+
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+
+    var text1 = doodlArray[(i * 2)];
+    var text2 = doodlArray[((i * 2) + 1)];
+
+    td1.appendChild(text1);
+    td2.appendChild(text2);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    table.appendChild(tr);
+}
+
+document.body.appendChild(table);
