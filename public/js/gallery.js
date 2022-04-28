0f428891504638a2c2c7cdf1d362159161ba133a
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-28 22:45:32 +0100

    line removed

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -75,7 +75,8 @@ window.onload=(function(){
 
         reportForm.appendChild(report);
         likeForm.appendChild(like);
-        dislikeForm.appendChild(dislike);
+     
+        
 
         var tr = document.createElement('tr');
         var tempArray = doodlArray[i].split('|', 2);
