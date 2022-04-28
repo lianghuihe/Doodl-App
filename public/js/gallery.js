fe8b921049d62c6e6b0eb6695c2496980d3e00db
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-28 20:05:22 +0100

    Table heading and canvas sizes

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -8,6 +8,20 @@ window.onload=(function(){
     var table = document.createElement('table');
     table.style.width = "100%";
 
+    var tableHeadingRow = document.createElement('tr');
+    var tableHeadingName = document.createElement('th');
+    var tableHeadingDoodl = document.createElement('th');
+    tableHeadingName.style.width = "20%";
+
+    var heading1 = document.createTextNode("Artist");
+    var heading2 = document.createTextNode("Doodl");
+
+    tableHeadingName.appendChild(heading1);
+    tableHeadingDoodl.appendChild(heading2);
+    tableHeadingRow.appendChild(tableHeadingName);
+    tableHeadingRow.appendChild(tableHeadingDoodl);
+    table.appendChild(tableHeadingRow);
+
     for (var i = 1; i < doodlArray.length; i++){
         var tr = document.createElement('tr');
         var tempArray = doodlArray[i].split('|', 2);
@@ -17,15 +31,15 @@ window.onload=(function(){
 
         let url = tempArray[1];
         let doodlCanvas = document.createElement('canvas');
-        doodlCanvas.style.border = "5px solid #000000";
-        doodlCanvas.style.position = "relative";
-        doodlCanvas.width = "300px";
-        doodlCanvas.height = "200px";
+        //doodlCanvas.style.border = "5px solid #000000";
+        //doodlCanvas.style.position = "relative";
+        //doodlCanvas.width = "300px";
+        //doodlCanvas.height = "200px";
 
         let ctx = doodlCanvas.getContext('2d');
         let img = new Image;
         img.onload = function(){
-            ctx.drawImage(img, 0, 0, doodlCanvas.width, doodlCanvas.height);
+            ctx.drawImage(img, 0, 0);
         };
         img.src = url;
 
