8d05301a27ef2ee4e3ed22d8f517d6096a645add
jakepwheeler <jakephilipwheeler@gmail.com>
2022-04-29 01:19:11 +0100

    dislikeID

--- routes/routing.js
+++ routes/routing.js
@@ -167,15 +167,15 @@ router.post("/like", checkAuthenticated, async (req, res) => {
 });
 
 router.post("/dislike", checkAuthenticated, async (req, res) => {
-  const likeFound = await Like.findOne({ username: req.user.name, doodlID: req.body.likeDoodlID, type: req.body.likeType});
+  const likeFound = await Like.findOne({ username: req.user.name, doodlID: req.body.dislikeDoodlID, type: req.body.dislikeType});
   if(likeFound){ 
     req.flash("error", "You have already given your opinion on that doodl");
     res.redirect("/gallery");
   }else{  
-    const differentLikeFound = await Like.findOne({ username: req.user.name, doodlID: req.body.likeDoodlID});  
+    const differentLikeFound = await Like.findOne({ username: req.user.name, doodlID: req.body.dislikeDoodlID});  
     if(differentLikeFound){
       try {
-        var conditions = {username: req.user.name, doodlID: req.body.likeDoodlID};
+        var conditions = {username: req.user.name, doodlID: req.body.dislikeDoodlID};
         var update = {type : "Dislike"};
         await Like.updateOne(conditions, update);
       } catch (error) {
@@ -188,7 +188,7 @@ router.post("/dislike", checkAuthenticated, async (req, res) => {
         const like = new Like({
           id: (await Like.find().count()) + 1,
           username: req.user.name,
-          doodlID: req.body.likeDoodlID,
+          doodlID: req.body.dislikeDoodlID,
           type: "Dislike",
         });
         await like.save();
