4a64770d0096cc9accd6af5d4387185d5b5d2b5e
Liam B <Liam.Butterick@hotmail.co.uk>
2022-05-03 11:16:02 +0100

    colouring text on all pages

--- public/js/voting.js
+++ public/js/voting.js
@@ -20,3 +20,45 @@ btn2.addEventListener('click', function() {
   this.classList.toggle('red');
   
 });
+
+try {
+  let bkgTemp = sessionStorage.getItem('backColorOption');
+  //console.log(bkgTemp);
+  selectBackground();
+} catch (error) {
+  console.log(error);
+  //console.log("no bkgCol stored.");
+}
+
+function selectBackground() {
+  let settingsSelect = parseInt(sessionStorage.getItem('backColorOption'));
+  var bkgCol = "#f67570";
+  var btnCol = "#1565c0";
+  let txtCol = "#ededed";
+  switch (settingsSelect) {
+    case 0:
+      bkgCol = "#f67570";
+      btnCol = "#1565c0";
+      txtCol = "#171717";
+      break;
+    case 1:
+      bkgCol = "#171717";
+      btnCol = "#ededed";
+      txtCol = "#ededed";
+      break;
+    case 2:
+      bkgCol = "#ededed";
+      btnCol = "#171717";
+      txtCol = "#171717";
+      break;
+    case 3:
+      bkgCol = "#1565c0";
+      btnCol = "#f67570";
+      txtCol = "#171717";
+      break;
+  }
+  document.body.style.backgroundColor = bkgCol;
+  //document.body.style.color = txtCol;
+
+  document.getElementById('votingHeader').style.color = txtCol;
+}
