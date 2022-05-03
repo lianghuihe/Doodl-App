d36b0f62531d46914be54db16dbc7bfd2edd332f
Liam B <Liam.Butterick@hotmail.co.uk>
2022-05-03 05:04:52 +0100

    allowing saved background to persist across pages

--- public/js/account.js
+++ public/js/account.js
@@ -81,7 +81,7 @@ window.onload=(function(){
 });
 
 let settingsSelect = document.getElementById('selecterBackground');
-if(settingsSelect)
+if(!settingsSelect)
 {
     console.log("settingsSelect was null...")
     //settingsSelect.selectedIndex = 0;
