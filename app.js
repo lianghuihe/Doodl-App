272da56f62d3113cc0fd973d58616b55654c6964
Rohan Sharma <rohansharma@rohans-air.home>
2022-05-03 14:21:41 +0100

    404 error

--- app.js
+++ app.js
@@ -80,11 +80,11 @@ app.use(function(err, req, res, next) {
 });
 
 // catch 404 and forward to error handler
-/*
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
-*/
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, {
